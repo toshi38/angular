2058a997881711297b6b723fcd185b59cd883c02
David <david.waller@svenskaspel.se>
2018-11-12 22:37:25 +0100

    add mocks

--- aio/src/jestGlobalMocks.ts
+++ aio/src/jestGlobalMocks.ts
@@ -12,26 +12,42 @@ const mock = () => {
 
 Object.defineProperty(window, 'localStorage', {value: mock()});
 Object.defineProperty(window, 'sessionStorage', {value: mock()});
+class Worker {
+  constructor(stringUrl) {
+    this.url = stringUrl;
+    this.onmessage = () => {};
+  }
+  postMessage(msg) {
+    this.onmessage(msg);
+  }
+}
+Object.defineProperty(window, "Worker", { value: Worker });
+Object.defineProperty(window.HTMLElement.prototype, "scrollIntoView", {
+  value: () => {}
+});
+Object.defineProperty(window, "scrollBy", {
+  value: () => {}
+});
 Object.defineProperty(document, 'doctype', {
-    value: '<!DOCTYPE html>'
+  value: '<!DOCTYPE html>'
 });
 Object.defineProperty(window, 'getComputedStyle', {
-    value: () => {
-        return {
-            display: 'none',
-            appearance: ['-webkit-appearance']
-        };
-    }
+  value: () => {
+      return {
+          display: 'none',
+          appearance: ['-webkit-appearance']
+      };
+  }
 });
 /**
- * ISSUE: https://github.com/angular/material2/issues/7101
- * Workaround for JSDOM missing transform property
- */
+* ISSUE: https://github.com/angular/material2/issues/7101
+* Workaround for JSDOM missing transform property
+*/
 Object.defineProperty(document.body.style, 'transform', {
-    value: () => {
-        return {
-            enumerable: true,
-            configurable: true,
-        };
-    },
+  value: () => {
+      return {
+          enumerable: true,
+          configurable: true,
+      };
+  },
 });
